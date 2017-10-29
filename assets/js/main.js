372d27f36be0e4d885855eb9df6ec511a235e714
Ivo Zinenda <izinenda@gmail.com>
2017-10-29 12:31:04 +0200

    mouse wheel plugin and new defaults

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,4 +29,32 @@ $( document ).ready(function(){
     }
   );
   $('ul.tabs').tabs();
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      loop:true,
+      nav:true,
+      margin:10,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },            
+          960:{
+              items:3
+          },
+          1200:{
+              items:4
+          }
+      }
+  });
+  owl.on('mousewheel', '.owl-stage', function (e) {
+      if (e.deltaY>0) {
+          owl.trigger('next.owl');
+      } else {
+          owl.trigger('prev.owl');
+      }
+      e.preventDefault();
+  });
 })
